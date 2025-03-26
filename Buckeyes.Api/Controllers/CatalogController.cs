dcaf43f97ac43189c96f45142bfb22f7f47530e6
mcgraw159 <mcgraw.159@osu.edu>
2025-03-26 16:06:11 -0400

    Revert "Finishing Part 2"

--- Buckeyes.Api/Controllers/CatalogController.cs
+++ Buckeyes.Api/Controllers/CatalogController.cs
@@ -32,28 +32,17 @@ namespace Buckeyes.Api.Controllers {
         }
 
 
-        /*[HttpGet("{id:int}")]
+        [HttpGet("{id:int}")]
         public IActionResult GetItem(int id) {
             var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
             item.Id = id;
-            return Ok(item);
-        }*/
-
-        [HttpGet("{id:int}")]
-        public IActionResult GetItem(int id) {
-            var item = _db.Items.Find(id);
-            if(item == null){
-                return NotFound();
-            }
             return Ok(item);
         }
 
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            _db.Items.Add(item);
-            _db.SaveChanges();
-            return Created($"/catalog/{item.Id}", item);
+            return Created("/catalog/42", item);
         }
 
         [HttpPost("{id:int}/ratings")]
--- Buckeyes.Data/StoreContext.cs
+++ Buckeyes.Data/StoreContext.cs
@@ -5,11 +5,5 @@ namespace Buckeyes.Data{
 public class StoreContext: DbContext{
     public StoreContext(DbContextOptions<StoreContext> options): base(options){ }
     public DbSet<Item> Items {get; set;}
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            DbInitializer.Initialize(builder);
-        }
     }
 }
