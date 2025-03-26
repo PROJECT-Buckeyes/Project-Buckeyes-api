9156a1593dc8a45b7dbb92c502e2e3e4a824f310
AmiyahLee <Lee.9746@buckeyemail.osu.edu>
2025-03-26 12:45:45 -0400

    Updated the Post method (PP. 25)

--- Buckeyes.Api/Controllers/CatalogController.cs
+++ Buckeyes.Api/Controllers/CatalogController.cs
@@ -51,7 +51,9 @@ namespace Buckeyes.Api.Controllers {
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            return Created("/catalog/42", item);
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
         }
 
         [HttpPost("{id:int}/ratings")]
