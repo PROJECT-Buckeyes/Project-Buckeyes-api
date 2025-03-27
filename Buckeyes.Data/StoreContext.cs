b6d30249debd8c68a434c399f45e3e65a55eddc1
mcgraw.159 <mcgraw.159@osu.edu>
2025-03-26 23:12:58 -0400

    Added Orders

--- Buckeyes.Data/StoreContext.cs
+++ Buckeyes.Data/StoreContext.cs
@@ -5,6 +5,7 @@ namespace Buckeyes.Data{
 public class StoreContext: DbContext{
     public StoreContext(DbContextOptions<StoreContext> options): base(options){ }
     public DbSet<Item> Items {get; set;}
+    public DbSet<Order> Orders {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
