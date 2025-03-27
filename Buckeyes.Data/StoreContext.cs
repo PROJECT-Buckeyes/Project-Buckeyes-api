8d1b4a90ce97c03f26fe4a3c49b07c9ca1b502fd
mcgraw.159 <mcgraw.159@osu.edu>
2025-03-26 23:19:51 -0400

    Part 10

--- Buckeyes.Data/StoreContext.cs
+++ Buckeyes.Data/StoreContext.cs
@@ -3,6 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Buckeyes.Data{
 public class StoreContext: DbContext{
+
+    public StoreContext CreateDBContext()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<StoreContext>()
+            .UseSqlite("Data Source=../Registrar.sqlite")
+               
+        return new StoreContext(options);
+    }
     public StoreContext(DbContextOptions<StoreContext> options): base(options){ }
     public DbSet<Item> Items {get; set;}
     public DbSet<Order> Orders {get; set;}
