db9ec5bb011505f781e518ffa56b03ff5be1fb18
mcgraw159 <mcgraw.159@osu.edu>
2025-03-26 23:50:10 -0400

    Revert "Revert 9 main"

--- Buckeyes.Api/Controllers/CatalogController.cs
+++ Buckeyes.Api/Controllers/CatalogController.cs
@@ -33,17 +33,28 @@ namespace Buckeyes.Api.Controllers {
         }
 
 
-        [HttpGet("{id:int}")]
+        /*[HttpGet("{id:int}")]
         public IActionResult GetItem(int id) {
             var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
             item.Id = id;
+            return Ok(item);
+        }*/
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id) {
+            var item = _db.Items.Find(id);
+            if(item == null){
+                return NotFound();
+            }
             return Ok(item);
         }
 
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            return Created("/catalog/42", item);
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
         }
 
         [HttpPost("{id:int}/ratings")]
